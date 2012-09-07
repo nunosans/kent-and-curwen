c2f04ba39a746a25a3c3180e94b9c24e58bd6fd4
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-09-07 08:51:00 +0100

    Initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,13 @@
+// Copyright 2012 Savant. All Rights Reserved.
+
+/**
+ * @fileoverview Kent & Curwen main website scripts.
+ * @author nuno@savantstudio.co.uk (Nuno Coelho Santos).
+ */
+
+$(document).ready(function() {
+
+  $('body').addClass('ready');
+  cover('#cover');
+
+});
