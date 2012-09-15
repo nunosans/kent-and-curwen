e84c4f95adf22e8fbe33481ad7fdfe0dc983ba91
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-09-15 22:54:02 +0100

    Loggin on JS modules

--- javascripts/module.cover.js
+++ javascripts/module.cover.js
@@ -7,17 +7,28 @@
 
 function cover(element) {
 
-  $el = $(element);
+  console.log('Initialized "cover"');
+
+  el = $(element);
 
   // Define this object's elements.
   var body = $('body');
-  var container = $el;
-  var $images = $el.find('img');
+  var container = el;
+  var images = el.find('img');
+  console.log('body: ');
+  console.log(body);
+  console.log('container: ');
+  console.log(container);
+  console.log('images: ');
+  console.log(images);
 
   // Define variables.
-  var imagesWidth = $images.width();
-  var imagesHeight = $images.height();
+  var imagesWidth = images.width();
+  var imagesHeight = images.height();
   var imagesRatio = imagesHeight / imagesWidth;
+  console.log('imagesWidth: ' + imagesWidth);
+  console.log('imagesHeight: ' + imagesHeight);
+  console.log('imagesRatio: ' + imagesRatio);
 
   // Required styles.
   body.css({
@@ -30,7 +41,7 @@ function cover(element) {
     'position': 'absolute',
     'top': 0
   });
-  $images.css({
+  images.css({
     'left': 0,
     'position': 'absolute',
     'top': 0
@@ -38,59 +49,81 @@ function cover(element) {
 
   function resize() {
 
+    console.log('Initialized "cover.resize"');
+
     // Redefine variables.
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
     var windowRatio = windowHeight / windowWidth;
+    console.log('windowWidth: ' + windowWidth);
+    console.log('windowHeight: ' + windowHeight);
+    console.log('windowRatio: ' + windowRatio);
 
     if (windowRatio > imagesRatio) {
 
+      console.log('Reizing to maximum height...');
+
       // Bleed on right and left.
-      $images.height(windowHeight);
-      $images.width($images.height() / imagesRatio);
+      images.height(windowHeight);
+      images.width('auto');
 
       container.width(windowWidth);
       container.height('100%');
 
-      $images.css('top', 0);
-      $images.css('left', - ($images.width() - windowWidth) / 2);
+      images.css('top', 0);
+      images.css('left', - (images.width() - windowWidth) / 2);
 
     } else {
 
+      console.log('Reizing to maximum width...');
+
       // Bleed on top and bottom.
-      $images.width(windowWidth);
-      $images.height(windowWidth * imagesRatio);
+      images.width(windowWidth);
+      images.height('auto');
 
       container.height(windowHeight);
       container.width('100%');
 
-      $images.css('left', 0);
-      $images.css('top', - ($images.height() - windowHeight) / 2);
+      images.css('left', 0);
+      images.css('top', - (images.height() - windowHeight) / 2);
 
     }
 
   };
 
-  function play() {
+  function swap() {
 
-    var $first = $el.find('img:first-child');
-    $first.addClass('active');
+    console.log('Initialized "cover.swap"');
+
+    var first = el.find('img:first-child');
+    console.log('first: ');
+    console.log(first);
+    first.addClass('active');
 
     setInterval(function() {
 
-      var $active = $el.find('img.active');
-      var $next = $active.next();
+      console.log('Swapping images...');
 
-      if (!$active.next().length) {
-        // alert('no next');
-        $next = $first;
+      var active = el.find('img.active');
+      var next = active.next();
+      console.log('active: ');
+      console.log(active);
+
+      if (!active.next().length) {
+        console.log('Restarting...');
+        next = first;
       };
 
+      console.log('next: ');
+      console.log(next);
+
+      console.log('Updating classes...')
+
       // Deactivate current active image.
-      $active.removeClass('active');
+      active.removeClass('active');
 
       // Make the next image active.
-      $next.addClass('active');
+      next.addClass('active');
 
     },
     7000);
@@ -99,7 +132,7 @@ function cover(element) {
 
   // Call functions.
   resize();
-  play();
+  swap();
 
   // Trigger resize also on window resize.
   $(window).resize(function() {
--- javascripts/module.pages.js
+++ javascripts/module.pages.js
@@ -7,7 +7,11 @@
 
 function pages(element) {
 
+  // console.log('Initialized "pages"');
+
   el = $(element);
+  // console.log('el:');
+  // console.log(el);
 
   var body = $('body');
   var sections = $('.section');
@@ -17,44 +21,86 @@ function pages(element) {
   var windowWidth = $(window).width();
   var windowHeight = $(window).height();
   var location = 'http://kentandcurwen.dev/';
+  var currentSection = window.location.search.split('#')[1];
+
+  // console.log('body:');
+  // console.log(body);
+  // console.log('sections:');
+  // console.log(sections);
+  // console.log('screens:');
+  // console.log(screens);
+  // console.log('navigation:');
+  // console.log(navigation);
+  // console.log('navigationItems:');
+  // console.log(navigationItems);
+  // console.log('windowWidth: ' + windowWidth);
+  // console.log('windowHeight: ' + windowHeight);
+  // console.log('location: ' + location);
+  // console.log('currentSection: ' + currentSection);
 
   function slide() {
+
+    // console.log('Initialized "pages.slide"');
 
     // Declare variables.
     var sectionName = $(this).attr('rel');
     var section = $('#' + sectionName);
     var index = $('span').index(this);
 
+    // console.log('sectionName: ' + sectionName);
+    // console.log('section: ' + section);
+    // console.log('index: ' + index);
+
     // If this is not the current section.
     if (!$(this).hasClass('active')){
 
+      // console.log('Switching section.');
+      // console.log('Clicked item: ' + $(this));
+
       // Remove the active class from the other items and add it to this one.
+      // console.log('Updating active nav item.')
       navigationItems.removeClass('active');
       $(this).addClass('active');
 
       // Slide to the right position.
+      // console.log('Animating body.');
       body.animate({
         'left': ('-' + (windowWidth * index) + 'px')
         },
-      800,
+      400,
       // Update the url.
       function() {
+        // console.log('Rewriting url with ' + location + '#' + sectionName);
         window.location.replace(location + '#' + sectionName);
       }
       );
     } else {
+
+      // console.log('Scrolling page to the top.');
+
       // Scroll to the top.
-      section.animate({scrollTop : 0}, 400);
+      section.animate({scrollTop : 0}, 1200);
     };
 
   };
 
   function resize() {
+
+    // console.log('Initialized "pages.resize"')
+
+    // Recalculate variables.
     windowWidth = $(window).width();
     windowHeight = $(window).height();
+    bodyWidth = windowWidth * sections.length;
+    // console.log('windowWidth: ' + windowWidth);
+    // console.log('windowHeight: ' + windowHeight);
+    // console.log('bodyWidth: ' + bodyWidth);
+
+    // Apply variables.
     sections.width(windowWidth);
     sections.height(windowHeight);
-    body.width(windowWidth * sections.length);
+    body.width(bodyWidth);
+
   };
 
   // Call functions.
