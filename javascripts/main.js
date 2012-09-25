a7a526ea893a49049a94e4ee2456886452bc3ca7
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-09-25 08:43:25 +0100

    Minifyed JS, fetching images from London Server [Revert this]

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,6 +5,304 @@
  * @author nuno@savantstudio.co.uk (Nuno Coelho Santos).
  */
 
+/**
+ * Function to update the image source based on image size.
+ */
+function resolute() {
+
+  // Declare variables.
+  var $images = $('img');
+  var width = $(window).width();
+  var res;
+  var size;
+
+  // Determine if it is a high or low resolution screen.
+  if (window.devicePixelRatio === 1) {
+    res = 'lo-res';
+  } else {
+    res = 'hi-res';
+  }
+
+  // Determine if it is a large, medium or small screen.
+  if (width > 1500) {
+    size = 'L';
+  } else if (width > 720) {
+    size = 'M';
+  } else {
+    size = 'S';
+  }
+
+  // Rewrite each image's source attribute.
+  $images.each(function() {
+      var src = $(this).attr('src').substring(17);
+      $(this).attr('src', ('http://media.savantstudio.co.uk/kentandcurwen/images/' + size + '/' + res + '/' + src));
+  });
+
+  // small/lo-res/  (maximum image width:  720px)
+  // small/hi-res/  (maximum image width: 1440px)
+  // medium/lo-res/ (maximum image width: 1440px)
+  // medium/hi-res/ (maximum image width: 2880px)
+  // large/lo-res/  (maximum image width: 2880px)
+  // large/hi-res/  (maximum image width: 5760px)
+
+}
+
+/**
+ * Function to generate the horizontal multi-page layout.
+ */
+function pages(element) {
+
+  el = $(element);
+
+  var body = $('body');
+  var sections = $('.section');
+  var screens = $('.parallax > *');
+  var navigation = $('#nav');
+  var navigationItems = navigation.find('span');
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
+  // var location = 'http://kentandcurwen.dev/';
+  // var location = 'http://kentandcurwen.co.uk/';
+  var location = 'http://test.kentandcurwen.co.uk/';
+  var currentSection = window.location.search.split('#')[1];
+
+  function slide() {
+
+
+    // Declare variables.
+    var sectionName = $(this).attr('rel');
+    var section = $('#' + sectionName);
+    var index = $('span').index(this);
+    var newsArticle = $('#news .article.sticky div.text');
+
+    // If this is not the current section.
+    if (!$(this).hasClass('active')){
+
+
+      // Remove the active class from the other items and add it to this one.
+      navigationItems.removeClass('active');
+      $(this).addClass('active');
+
+      // Slide to the right position.
+      body.animate({
+        'left': ('-' + (windowWidth * index) + 'px')
+        },
+      400
+      // function() {
+        // Update the url.
+        // if (currentSection !== 'undefined') {
+          // window.location.replace(location + '#' + sectionName);
+        // };
+      // }
+      );
+      newsArticle.animate({
+        'left': ( -(windowWidth * (index - 3)) + 'px')
+        },
+      400
+      );
+
+      // Update nav
+      updateNav(section);
+
+    } else {
+
+      // Scroll to the top.
+      section.animate({scrollTop : 0}, 1200);
+
+    };
+
+  };
+
+  function updateNav(element) {
+
+    if (element instanceof jQuery === false) {
+      element = $(this);
+    };
+
+    var sectionName = element.attr('id');
+
+    if( element.scrollTop() > 50 ) {
+      navigation.removeClass('top');
+      navigation.addClass('hidden');
+    } else {
+      if (sectionName === 'news') {
+        navigation.removeClass('top');
+      } else {
+        navigation.addClass('top');
+      }
+      navigation.removeClass('hidden');
+    }
+  };
+
+  function resize() {
+
+    // Recalculate variables.
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+    bodyWidth = windowWidth * sections.length;
+
+    // Apply variables.
+    sections.width(windowWidth);
+    sections.height(windowHeight);
+    body.width(bodyWidth);
+
+  };
+
+  // Call functions.
+  sections.scroll(updateNav);
+  navigationItems.click(slide);
+  resize();
+
+  // Call functions on window resize.
+  $(window).resize(function() {
+    resize();
+  });
+
+}
+
+/**
+ * Function to generate cover artwork animation.
+ */
+function cover(element) {
+
+  el = $(element);
+
+  // Define this object's elements.
+  var body = $('body');
+  var container = el;
+  var images = el.find('img');
+
+  // Define variables.
+  var imagesWidth = images.width();
+  var imagesHeight = images.height();
+  var imagesRatio = imagesHeight / imagesWidth;
+
+  // Required styles.
+  body.css({
+    'margin': 0,
+    'overflow': 'hidden'
+  });
+  container.css({
+    'left': 0,
+    'overflow': 'hidden',
+    'position': 'absolute',
+    'top': 0
+  });
+  images.css({
+    'left': 0,
+    'position': 'absolute',
+    'top': 0
+  });
+
+  function resize() {
+
+    // Redefine variables.
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    var windowRatio = windowHeight / windowWidth;
+
+    if (windowRatio > imagesRatio) {
+
+      // Bleed on right and left.
+      images.height(windowHeight);
+      images.width('auto');
+
+      container.width(windowWidth);
+      container.height('100%');
+
+      images.css('top', 0);
+      images.css('left', - (images.width() - windowWidth) / 2);
+
+    } else {
+
+      // Bleed on top and bottom.
+      images.width(windowWidth);
+      images.height('auto');
+
+      container.height(windowHeight);
+      container.width('100%');
+
+      images.css('left', 0);
+      images.css('top', - (images.height() - windowHeight) / 2);
+
+    }
+
+  };
+
+  function swap() {
+
+    var first = el.find('img:first-child');
+    first.addClass('active');
+
+    setInterval(function() {
+
+      var active = el.find('img.active');
+      var next = active.next();
+
+      if (!active.next().length) {
+        next = first;
+      };
+
+      // Deactivate current active image.
+      active.removeClass('active');
+
+      // Make the next image active.
+      next.addClass('active');
+
+    },
+    7000);
+
+  };
+
+  // Call functions.
+  resize();
+  swap();
+
+  // Trigger resize also on window resize.
+  $(window).resize(function() {
+    resize();
+  });
+
+}
+
+/**
+ * Initialize functions when DOM is ready.
+ */
+function dropdowns(element) {
+
+  el = $(element);
+  var mainSection = el.find('h3');
+  // var subSection = el.find('h4');
+
+  function init() {
+    // subSection.each(function() {
+    //   var parent = $(this).parent('li');
+    //   var parentHeight = $(this).parent('li').height();
+    //   parent.height(parentHeight).addClass('closed');
+    // });
+    mainSection.each(function() {
+      var parent = $(this).parent('li');
+      var parentHeight = $(this).parent('li').height();
+      parent.height(parentHeight).addClass('closed');
+    });
+  };
+
+  function set() {
+    var parent = $(this).parent('li');
+    parent.toggleClass('closed');
+
+  };
+
+  // Call functions.
+  init();
+  mainSection.click(set);
+  // subSection.click(set);
+
+};
+
+/**
+ * Initialize functions when DOM is ready.
+ */
 $(document).ready(function() {
 
   // Rewrite the appropriate images path.
