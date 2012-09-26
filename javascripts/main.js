d594e355e4f8e377216bd181056e32707005d51a
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-09-26 20:31:21 +0100

    PHP-free version

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,160 +5,6 @@
  * @author nuno@savantstudio.co.uk (Nuno Coelho Santos).
  */
 
-/**
- * Function to update the image source based on image size.
- */
-function resolute() {
-
-  // Declare variables.
-  var $images = $('img');
-  var width = $(window).width();
-  var res;
-  var size;
-
-  // Determine if it is a high or low resolution screen.
-  if (window.devicePixelRatio === 1) {
-    res = 'lo-res';
-  } else {
-    res = 'hi-res';
-  }
-
-  // Determine if it is a large, medium or small screen.
-  if (width > 1500) {
-    size = 'L';
-  } else if (width > 720) {
-    size = 'M';
-  } else {
-    size = 'S';
-  }
-
-  // Rewrite each image's source attribute.
-  $images.each(function() {
-      var src = $(this).attr('src').substring(17);
-      $(this).attr('src', ('http://media.savantstudio.co.uk/kentandcurwen/images/' + size + '/' + res + '/' + src));
-  });
-
-  // small/lo-res/  (maximum image width:  720px)
-  // small/hi-res/  (maximum image width: 1440px)
-  // medium/lo-res/ (maximum image width: 1440px)
-  // medium/hi-res/ (maximum image width: 2880px)
-  // large/lo-res/  (maximum image width: 2880px)
-  // large/hi-res/  (maximum image width: 5760px)
-
-}
-
-/**
- * Function to generate the horizontal multi-page layout.
- */
-function pages(element) {
-
-  el = $(element);
-
-  var body = $('body');
-  var sections = $('.section');
-  var screens = $('.parallax > *');
-  var navigation = $('#nav');
-  var navigationItems = navigation.find('span');
-  var windowWidth = $(window).width();
-  var windowHeight = $(window).height();
-  // var location = 'http://kentandcurwen.dev/';
-  // var location = 'http://kentandcurwen.co.uk/';
-  var location = 'http://test.kentandcurwen.co.uk/';
-  var currentSection = window.location.search.split('#')[1];
-
-  function slide() {
-
-
-    // Declare variables.
-    var sectionName = $(this).attr('rel');
-    var section = $('#' + sectionName);
-    var index = $('span').index(this);
-    var newsArticle = $('#news .article.sticky div.text');
-
-    // If this is not the current section.
-    if (!$(this).hasClass('active')){
-
-
-      // Remove the active class from the other items and add it to this one.
-      navigationItems.removeClass('active');
-      $(this).addClass('active');
-
-      // Slide to the right position.
-      body.animate({
-        'left': ('-' + (windowWidth * index) + 'px')
-        },
-      400
-      // function() {
-        // Update the url.
-        // if (currentSection !== 'undefined') {
-          // window.location.replace(location + '#' + sectionName);
-        // };
-      // }
-      );
-      newsArticle.animate({
-        'left': ( -(windowWidth * (index - 3)) + 'px')
-        },
-      400
-      );
-
-      // Update nav
-      updateNav(section);
-
-    } else {
-
-      // Scroll to the top.
-      section.animate({scrollTop : 0}, 1200);
-
-    };
-
-  };
-
-  function updateNav(element) {
-
-    if (element instanceof jQuery === false) {
-      element = $(this);
-    };
-
-    var sectionName = element.attr('id');
-
-    if( element.scrollTop() > 50 ) {
-      navigation.removeClass('top');
-      navigation.addClass('hidden');
-    } else {
-      if (sectionName === 'news') {
-        navigation.removeClass('top');
-      } else {
-        navigation.addClass('top');
-      }
-      navigation.removeClass('hidden');
-    }
-  };
-
-  function resize() {
-
-    // Recalculate variables.
-    windowWidth = $(window).width();
-    windowHeight = $(window).height();
-    bodyWidth = windowWidth * sections.length;
-
-    // Apply variables.
-    sections.width(windowWidth);
-    sections.height(windowHeight);
-    body.width(bodyWidth);
-
-  };
-
-  // Call functions.
-  sections.scroll(updateNav);
-  navigationItems.click(slide);
-  resize();
-
-  // Call functions on window resize.
-  $(window).resize(function() {
-    resize();
-  });
-
-}
 
 /**
  * Function to generate cover artwork animation.
@@ -314,7 +160,6 @@ $(document).ready(function() {
 
     // Call modules.
     cover('#home .cover');
-    pages('.section');
     dropdowns('#stores > ul');
 
     $('body').stickySectionHeaders({
@@ -322,7 +167,7 @@ $(document).ready(function() {
       headlineSelector: '.text'
     });
 
-    $('.section').stellar({
+    $('.parallax').stellar({
       hideDistantElements: false,
       verticalOffset: 0
     });
