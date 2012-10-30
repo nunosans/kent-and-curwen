005cc065a30d64bf594d82261269a2a97b4aba22
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-10-30 17:40:54 +0000

    Animating parallax with webkit transform matrix rather than with javascript absolute positioning

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,42 +5,57 @@
  */
 $(document).ready(function() {
 
+  // Declare variables.
+  var body = $('body');
+
   // Add mobile or desktop class.
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-    $('body').addClass('mobile');
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    body.addClass('mobile');
   } else {
-    $('body').addClass('desktop');
+    body.addClass('desktop');
   };
 
   // Calculate correct height of the body.
-  $('body').height($(window).height());
+  body.height($(window).height());
+
+  // If the screen is in portrait, add class portrait.
+  if (body.height() > (body.width() * 0.71)) {
+    body.addClass('portrait');
+  };
 
   // Call modules.
   dropdowns('body.stores > ul');
 
-  $('body.news').stickySectionHeaders({
-    stickyClass: 'sticky',
-    headlineSelector: '.text'
-  });
+  // Apply sticky section headers.
+  if (body.hasClass('news')) {
+    body.stickySectionHeaders({
+      stickyClass: 'sticky',
+      headlineSelector: '.text'
+    });
+  };
 
-  $('body.desktop').stellar({
-    hideDistantElements: false,
-    verticalOffset: 0
-  });
+  // Apply parallax effect.
+  if (body.hasClass('parallax')) {
+    body.stellar({
+      positionProperty: 'transform',
+      hideDistantElements: false,
+      verticalOffset: 0,
+      parallaxBackgrounds: false
+    });
+  };
 
+  // Scroll to top.
   $('#nav a.current').click(function() {
-    $('body').animate({scrollTop : 0}, 1200);
+    body.animate({scrollTop : 0}, 1200);
   });
-
-
 
   $(window).load(function() {
 
     // Add class ready to the body.
-    $('body').addClass('ready');
+    body.addClass('ready');
 
     // Call the cover module if this is the landing page.
-    if ($('body').hasClass('cover')) {
+    if (body.hasClass('cover')) {
       cover('.cover');
     } else {
       $('.spinner').children().hide().end().fadeOut(600);
@@ -49,7 +64,16 @@ $(document).ready(function() {
   });
 
   $(window).resize(function() {
-    $('body').height($(window).height());
+
+    // Re-apply the correct height of the body.
+    body.height($(window).height());
+
+    // Check if the screen is now in portrait.
+    if (body.height() > (body.width() * 0.71)) {
+      body.addClass('portrait');
+    } else {
+      body.removeClass('portrait');
+    }
   });
 
 });
